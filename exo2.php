e6f369eb801bea63a757bdc15bc2321050c93986
dev3459 <test45@outlook.fr>
2021-02-02 13:26:17 +0100

    Exo 154 Fini

--- exo2.php
+++ exo2.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_GET['iteration']) && $_GET['iteration'] === '10'){
     for($i = 0; $i < $_GET['iteration']; $i++){
-        echo "Hello Wolrd".$i. "<br>";
+        echo "<a href='exo1.php?iteration=$i' title='$i'>$i</a><br>";
     }
 }else{
     header("Location:index.php?error=1");
